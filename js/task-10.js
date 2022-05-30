0839ad94667ff4ae152981de26e1c16d7b401d01
VitaliiPozniakov <vertalen89@gmail.com>
2022-05-30 12:14:21 +0300

    Finally

--- js/task-10.js
+++ js/task-10.js
@@ -2,40 +2,35 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const buttonCreateRef = document.querySelector(`button[data-create]`);
+const buttonClearRef = document.querySelector(`button[data-destroy]`);
+const inputRef = document.querySelector(`input[type="number"]`);
+const containerRef = document.querySelector(`#boxes`);
 
-const buttonCreateRef = document.querySelector(`button[data-create]`)
-const buttonClearRef = document.querySelector(`button[data-destroy]`)
-const inputRef = document.querySelector(`input[type="number"]`)
-const containerRef = document.querySelector(`#boxes`)
-
-
-function onButtonCreateClick (event) {
-  createBoxes(inputRef.value)
+function onButtonCreateClick(event) {
+  createBoxes(inputRef.value);
   // console.log(inputRef.value)
 }
 
-
 function createBoxes(amount) {
-  const basicSize = 30;
+  const baseSize = 30;
   const elements = [];
   for (let i = 0; i < amount; i += 1) {
-    const size = basicSize + i * 10;
-    const itemRef = document.createElement('div');
-    itemRef.style.cssText = `width: ${size}px; height: ${size}px;`;
+    const size = baseSize + i * 10;
+    const itemRef = document.createElement("div");
+    itemRef.style.width = `${size}px`;
+    itemRef.style.height = `${size}px`;
+    //  itemRef.style.cssText = `width: ${size}px; height: ${size}px;`;
     itemRef.style.backgroundColor = `${getRandomHexColor()}`;
 
-    elements.push(itemRef)
-  }
-  containerRef.append(...elements)
 
+    elements.push(itemRef);
+  }
+  containerRef.append(...elements);
 }
 
-
-
-
-buttonCreateRef.addEventListener(`click`, onButtonCreateClick)
-
+buttonCreateRef.addEventListener(`click`, onButtonCreateClick);
 
 buttonClearRef.addEventListener(`click`, () => {
-  containerRef.innerHTML = ''
-})
+  containerRef.innerHTML = "";
+});
